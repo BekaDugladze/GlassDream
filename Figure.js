45a4f23e76b15428e3ed23ac509f8143b18e2a1b
BekaDugladze <dugladzebeqa@gmail.com>
2023-03-17 12:14:17 +0400

    Renew

---
+++ Figure.js
@@ -0,0 +1,438 @@
+//Ardakan
+
+const ci = document.querySelector("#ardakanD");
+const ap = document.querySelector('#AP');
+const aa = document.querySelector('#AA');
+
+ci.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    ci.classList.add('move-c');
+    ap.classList.add('show-p')
+})
+
+ci.addEventListener('mouseleave', () => {
+    ci.classList.remove('move-c');
+    ap.classList.remove('show-p');
+})
+
+ap.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  ci.classList.add('move-c');
+  ap.classList.add('show-p');
+})
+
+ap.addEventListener('mouseleave', () => {
+  ci.classList.remove('move-c');
+  ap.classList.remove('show-p');
+})
+
+aa.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  ci.classList.add('move-c');
+  ap.classList.add('show-p');
+})
+
+aa.addEventListener('mouseleave', () => {
+  ci.classList.remove('move-c');
+  ap.classList.remove('show-p');
+})
+
+//ArdakanEnd
+
+//AGC
+
+const agcf = document.querySelector("#AGC");
+const agcp = document.querySelector('#AGCP');
+const agca = document.querySelector('#AGCA');
+
+agcf.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    agcf.classList.add('move-l');
+    agcp.classList.add('show-p');
+})
+
+agcf.addEventListener('mouseleave', () => {
+    agcf.classList.remove('move-l');
+    agcp.classList.remove('show-p');
+})
+
+agcp.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  agcf.classList.add('move-l');
+  agcp.classList.add('show-p');
+})
+
+agcp.addEventListener('mouseleave', () => {
+  agcf.classList.remove('move-l');
+  agcp.classList.remove('show-p');
+})
+
+agca.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  agcf.classList.add('move-l');
+  agcp.classList.add('show-p');
+})
+
+agca.addEventListener('mouseleave', () => {
+  agcf.classList.remove('move-l');
+  agcp.classList.remove('show-p');
+})
+
+//AGCEnd
+
+//AZ
+
+const az = document.querySelector("#AZ");
+const azp = document.querySelector('#AZP');
+const aza = document.querySelector('#AZA');
+
+az.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    az.classList.add('move-c');
+    azp.classList.add('show-p');
+})
+
+az.addEventListener('mouseleave', () => {
+    az.classList.remove('move-c');
+    azp.classList.remove('show-p');
+})
+
+azp.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  az.classList.add('move-c');
+  azp.classList.add('show-p');
+})
+
+azp.addEventListener('mouseleave', () => {
+  az.classList.remove('move-c');
+  azp.classList.remove('show-p');
+})
+
+aza.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  az.classList.add('move-c');
+  azp.classList.add('show-p');
+})
+
+aza.addEventListener('mouseleave', () => {
+  az.classList.remove('move-c');
+  azp.classList.remove('show-p');
+})
+
+//AZEnd
+
+//SGG
+
+const sgg = document.querySelector("#SGG");
+const sgp = document.querySelector('#SGP');
+const sga = document.querySelector('#SGA');
+
+sgg.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    sgg.classList.add('move-l');
+    sgp.classList.add('show-p');
+})
+
+sgg.addEventListener('mouseleave', () => {
+    sgg.classList.remove('move-l');
+    sgp.classList.remove('show-p');
+})
+
+sgp.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  sgg.classList.add('move-l');
+  sgp.classList.add('show-p');
+})
+
+sgp.addEventListener('mouseleave', () => {
+  sgg.classList.remove('move-l');
+  sgp.classList.remove('show-p');
+})
+
+sga.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  sgg.classList.add('move-l');
+  sgp.classList.add('show-p');
+})
+
+sga.addEventListener('mouseleave', () => {
+  sgg.classList.remove('move-l');
+  sgp.classList.remove('show-p');
+})
+
+//SGGEnd
+
+//GG
+
+const gg = document.querySelector("#GG");
+const gp = document.querySelector('#GP');
+const ga = document.querySelector('#GA');
+
+gg.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    gg.classList.add('move-c');
+    gp.classList.add('show-p');
+})
+
+gg.addEventListener('mouseleave', () => {
+    gg.classList.remove('move-c');
+    gp.classList.remove('show-p');
+})
+
+gp.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  gg.classList.add('move-c');
+  gp.classList.add('show-p');
+})
+
+gp.addEventListener('mouseleave', () => {
+  gg.classList.remove('move-c');
+  gp.classList.remove('show-p');
+})
+
+ga.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  gg.classList.add('move-c');
+  gp.classList.add('show-p');
+})
+
+ga.addEventListener('mouseleave', () => {
+  gg.classList.remove('move-c');
+  gp.classList.remove('show-p');
+})
+
+//GGEnd
+
+//SS
+const ss = document.querySelector("#SS");
+const sp = document.querySelector('#SP');
+const sa = document.querySelector('#SA');
+
+ss.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    ss.classList.add('move-l');
+    sp.classList.add('show-p');
+})
+
+ss.addEventListener('mouseleave', () => {
+    ss.classList.remove('move-l');
+    sp.classList.remove('show-p');
+})
+
+sp.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  ss.classList.add('move-l');
+  sp.classList.add('show-p');
+})
+
+sp.addEventListener('mouseleave', () => {
+  ss.classList.remove('move-l');
+  sp.classList.remove('show-p');
+})
+
+sa.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  ss.classList.add('move-l');
+  sp.classList.add('show-p');
+})
+
+sa.addEventListener('mouseleave', () => {
+  ss.classList.remove('move-l');
+  sp.classList.remove('show-p');
+})
+
+//SSEnd
+
+//KA
+
+const ka = document.querySelector("#KA");
+const kp = document.querySelector('#KP');
+const kaa = document.querySelector('#KAA');
+
+ka.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    ka.classList.add('move-c');
+    kp.classList.add('show-p');
+})
+
+ka.addEventListener('mouseleave', () => {
+    ka.classList.remove('move-c');
+    kp.classList.remove('show-p');
+})
+
+kp.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  ka.classList.add('move-c');
+  kp.classList.add('show-p');
+})
+
+kp.addEventListener('mouseleave', () => {
+  ka.classList.remove('move-c');
+  kp.classList.remove('show-p');
+})
+
+kaa.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  ka.classList.add('move-c');
+  kp.classList.add('show-p');
+})
+
+kaa.addEventListener('mouseleave', () => {
+  ka.classList.remove('move-c');
+  kp.classList.remove('show-p');
+})
+
+//KAEnd
+
+//SI
+
+const si = document.querySelector("#SI");
+const sip = document.querySelector('#SIP');
+const sia = document.querySelector('#SIA');
+
+si.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    si.classList.add('move-l');
+    sip.classList.add('show-p');
+})
+
+si.addEventListener('mouseleave', () => {
+    si.classList.remove('move-l');
+    sip.classList.remove('show-p');
+})
+
+sip.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  si.classList.add('move-l');
+  sip.classList.add('show-p');
+})
+
+sip.addEventListener('mouseleave', () => {
+  si.classList.remove('move-l');
+  sip.classList.remove('show-p');
+})
+
+sia.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  si.classList.add('move-l');
+  sip.classList.add('show-p');
+})
+
+sia.addEventListener('mouseleave', () => {
+  si.classList.remove('move-l');
+  sip.classList.remove('show-p');
+})
+
+//SIEnd
+
+//RE
+
+const re = document.querySelector("#RE");
+const rep = document.querySelector('#REP');
+const rea = document.querySelector('#REA');
+
+re.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    re.classList.add('move-c');
+    rep.classList.add('show-p');
+})
+
+re.addEventListener('mouseleave', () => {
+    re.classList.remove('move-c');
+    rep.classList.remove('show-p');
+})
+
+rep.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  re.classList.add('move-c');
+  rep.classList.add('show-p');
+})
+
+rep.addEventListener('mouseleave', () => {
+  re.classList.remove('move-c');
+  rep.classList.remove('show-p');
+})
+
+rea.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  re.classList.add('move-c');
+  rep.classList.add('show-p');
+})
+
+rea.addEventListener('mouseleave', () => {
+  re.classList.remove('move-c');
+  rep.classList.remove('show-p');
+})
+
+//REEnd
+
+//INC
+
+const inc = document.querySelector("#IN");
+const inp = document.querySelector( '#INP');
+const ina = document.querySelector( '#INA');
+
+inc.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    inc.classList.add('move-l');
+    inp.classList.add('show-p');
+})
+
+inc.addEventListener('mouseleave', () => {
+    inc.classList.remove('move-l');
+    inp.classList.remove('show-p');
+})
+
+inp.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  inc.classList.add('move-l');
+  inp.classList.add('show-p');
+})
+
+inp.addEventListener('mouseleave', () => {
+  inc.classList.remove('move-l');
+  inp.classList.remove('show-p');
+})
+
+ina.addEventListener('mouseover', (e) => {
+  console.log('works')
+
+  inc.classList.add('move-l');
+  inp.classList.add('show-p');
+})
+
+ina.addEventListener('mouseleave', () => {
+  inc.classList.remove('move-l');
+  inp.classList.remove('show-p');
+})
+
+//INCEnd
--- javascript.js
+++ javascript.js
@@ -8,12 +8,48 @@ gamotvla.onclick = function() {
     let sigane = document.getElementById('sigane').value;
     let raodenoba = document.getElementById('raodenoba').value;
 
-    const kvadrati = Math.floor(simagle * sigane * raodenoba);
+    const kvadrati = Math.round(100 * simagle * sigane * raodenoba) / 100; //Measedebamde damrgvaleba!
 
-    if(kvadrati > 0) {
-        return alert(`მოცემული პროდუქტი არის ${kvadrati} კვადრატი.`)
+    let mina = document.getElementById('glassSelect').value;
+    let fasi = 0;
+    switch (mina){
+        case 'guardian4':
+            fasi = 4.1;
+            break;
+        case 'guardian6':
+            fasi = 6;
+            break;
+        case 'guardian8':
+            fasi = 8.5;
+            break;
+        default:
+            fasi = 0;
+            break;
+    }
+
+    let par = document.getElementById('pasuxi')
+
+    if(kvadrati > 0 && mina !== '') {
+        return par.innerHTML = `მოცემული პროდუქტი არის ${kvadrati} კვადრატი. პროდუქტის ფასია ${Math.round(100 * fasi * kvadrati) / 100} დოლარი.`;
+        /*let paragraph = document.createElement("P");
+      paragraph.innerHTML = "This is paragraph "+x;
+      document.getElementById("SampleDIV").appendChild(paragraph);*/
+    }
+    else if (kvadrati > 0 && mina === '') {
+        return par.innerHTML = `მოცემული პროდუქტი არის ${kvadrati} კვადრატი.`;
     }
     else{
-        return alert(`Error!`)
+        return par.innerHTML = `გთხოვთ, მიუთითოთ შესაბამისი მნიშვნელობა.`
     }
+
+
 }
+/*const ci = document.querySelector(".figure");
+
+ci.addEventListener("mouseover", (e) => {
+    console.log('works')
+
+    ci.classList.add('move-c');})
+    
+ci.addEventListener('mouseleave', () => {
+        ci.classList.remove('move-c');})*/
